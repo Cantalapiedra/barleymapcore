68fd15e6486978e87f5432bb3de91235f93d65ef
Cantalapiedra <cpcantalapiedra@gmail.co>
2016-11-24 10:14:07 +0100

    data_utils functions to db package classes

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -23,102 +23,6 @@ def read_paths(config_file_path): # TODO pass this to utils package
     
     return config_path_dict
 
-## NEXT CODE IS FIDDLY. WOULD BE BETTER WITH A SORTED DICT
-# Can be used to load databases, datasets, everything that is
-# configured in a conf_file with key-->value
-# and the user choice is a list of "," or default all
-def load_data(conf_file, users_list = None, verbose = False):
-    databases_list = []
-    databases_param = []
-    
-    if verbose: sys.stderr.write("Configured data:\n")
-    
-    databases_param_default = []
-    databases_param_default_ids = []
-    databases_configured = {}
-    for database in open(conf_file, 'r'):
-        database_data = database.strip().split(" ")
-        databases_param_default.append(database_data[0])
-        databases_param_default_ids.append(database_data[1])
-        databases_configured[database_data[0]] = database_data[1]
-        
-        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
-    
-    if users_list:
-        databases = users_list.split(",")
-        for database in databases:
-            if database in databases_configured:
-                databases_list.append(databases_configured[database])
-                databases_param.append(database)
-            else:
-                raise m2pException("Warning: data "+database+" is not present in config file.")
-        
-        databases_param = ",".join(databases_param)
-    else:
-        databases_list = databases_param_default_ids
-        databases_param = ",".join(databases_param_default)
-        
-    return (databases_param, databases_list)
-
-def load_ids(conf_file, users_list = None, verbose = False):
-    databases_list = []
-    databases_param = []
-    
-    if verbose: sys.stderr.write("Configured data:\n")
-    
-    databases_param_default = []
-    databases_param_default_ids = []
-    databases_configured = {}
-    for database in open(conf_file, 'r'):
-        database_data = database.strip().split(" ")
-        databases_param_default.append(database_data[0])
-        databases_param_default_ids.append(database_data[1])
-        databases_configured[database_data[1]] = database_data[0]
-        
-        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
-    
-    if users_list:
-        databases = users_list.split(",")
-        for database in databases:
-            if database in databases_configured:
-                databases_list.append(database)
-                databases_param.append(databases_configured[database])
-            else:
-                databases_list.append(database)
-                databases_param.append(database)
-                #raise m2pException("Warning: data "+database+" is not present in config file.")
-        
-        databases_param = ",".join(databases_param)
-    else:
-        databases_list = databases_param_default_ids
-        databases_param = ",".join(databases_param_default)
-        
-    return (databases_param, databases_list)
-
-def load_name(conf_file, user_name, verbose = False):
-    databases_list = []
-    databases_param = []
-    
-    if verbose: sys.stderr.write("Configured data:\n")
-    
-    databases_param_default = []
-    databases_configured = {}
-    for database in open(conf_file, 'r'):
-        database_data = database.strip().split(" ")
-        databases_param_default.append(database_data[0])
-        databases_configured[database_data[0]] = database_data[1]
-        
-        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
-    
-    database = user_name
-    if database in databases_configured:
-        database_id = databases_configured[database]
-        database_name = database
-    else:
-        raise m2pException("Warning: data "+database+" is not present in config file.")
-        
-    return (database_name, database_id)
-
 def load_conf(conf_file, verbose = False):
     conf_rows = []
     
@@ -132,6 +36,104 @@ def load_conf(conf_file, verbose = False):
         conf_rows.append(line_data)
     
     return conf_rows
+
+## NEXT CODE IS FIDDLY. WOULD BE BETTER WITH A SORTED DICT
+# Can be used to load databases, datasets, everything that is
+# configured in a conf_file with key-->value
+# and the user choice is a list of "," or default all
+#def load_data(conf_file, users_list = None, verbose = False):
+#    databases_list = []
+#    databases_param = []
+#    
+#    if verbose: sys.stderr.write("Configured data:\n")
+#    
+#    databases_param_default = []
+#    databases_param_default_ids = []
+#    databases_configured = {}
+#    for database in open(conf_file, 'r'):
+#        database_data = database.strip().split(" ")
+#        databases_param_default.append(database_data[0])
+#        databases_param_default_ids.append(database_data[1])
+#        databases_configured[database_data[0]] = database_data[1]
+#        
+#        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
+#    
+#    if users_list:
+#        databases = users_list.split(",")
+#        for database in databases:
+#            if database in databases_configured:
+#                databases_list.append(databases_configured[database])
+#                databases_param.append(database)
+#            else:
+#                raise m2pException("Warning: data "+database+" is not present in config file.")
+#        
+#        databases_param = ",".join(databases_param)
+#    else:
+#        databases_list = databases_param_default_ids
+#        databases_param = ",".join(databases_param_default)
+#        
+#    return (databases_param, databases_list)
+#
+#def load_ids(conf_file, users_list = None, verbose = False):
+#    databases_list = []
+#    databases_param = []
+#    
+#    if verbose: sys.stderr.write("Configured data:\n")
+#    
+#    databases_param_default = []
+#    databases_param_default_ids = []
+#    databases_configured = {}
+#    for database in open(conf_file, 'r'):
+#        database_data = database.strip().split(" ")
+#        databases_param_default.append(database_data[0])
+#        databases_param_default_ids.append(database_data[1])
+#        databases_configured[database_data[1]] = database_data[0]
+#        
+#        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
+#    
+#    if users_list:
+#        databases = users_list.split(",")
+#        for database in databases:
+#            if database in databases_configured:
+#                databases_list.append(database)
+#                databases_param.append(databases_configured[database])
+#            else:
+#                databases_list.append(database)
+#                databases_param.append(database)
+#                #raise m2pException("Warning: data "+database+" is not present in config file.")
+#        
+#        databases_param = ",".join(databases_param)
+#    else:
+#        databases_list = databases_param_default_ids
+#        databases_param = ",".join(databases_param_default)
+#        
+#    return (databases_param, databases_list)
+
+#def load_name(conf_file, user_name, verbose = False):
+#    databases_list = []
+#    databases_param = []
+#    
+#    if verbose: sys.stderr.write("Configured data:\n")
+#    
+#    databases_param_default = []
+#    databases_configured = {}
+#    for database in open(conf_file, 'r'):
+#        database_data = database.strip().split(" ")
+#        databases_param_default.append(database_data[0])
+#        databases_configured[database_data[0]] = database_data[1]
+#        
+#        if verbose: sys.stderr.write("\t"+database_data[0]+" --> ID: "+database_data[1]+"\n")
+#    
+#    database = user_name
+#    if database in databases_configured:
+#        database_id = databases_configured[database]
+#        database_name = database
+#    else:
+#        raise m2pException("Warning: data "+database+" is not present in config file.")
+#        
+#    return (database_name, database_id)
+
+
 
 #def load_datasets(conf_file, verbose = False):
 #    datasets_dict = {}
